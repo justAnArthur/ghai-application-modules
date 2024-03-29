3c1b78ce33a9d58995b798c9357041f96c7bdc64
Your Name <you@example.com>
2024-03-29 20:50:19 +0100

    Remove useless files after merge

--- server/src/main/java/fiit/vava/server/infrastructure/DBConnection.java
+++ server/src/main/java/fiit/vava/server/infrastructure/DBConnection.java
@@ -1,18 +1,21 @@
 package fiit.vava.server.infrastructure;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/db";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/db";
+    private static final String URL = Dotenv.load().get("DATABASE_URL");
     private static final String USER = "pastgres";
     private static final String PASSWORD = "postgres";
 
     public static Connection getConnection() throws SQLException {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL);
         } catch (final SQLException e) {
             // Log the error or throw a custom exception
             throw new SQLException("Failed to connect to the database", e);
